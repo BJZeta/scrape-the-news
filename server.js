715ccea8c360590f59ed7937ee0deca8b9b72c7c
Brandon U <unzueta.brandon@gmai.com>
2019-09-21 18:58:38 -0700

    create route to scrape comicbook.com articles

--- server.js
+++ server.js
@@ -21,10 +21,21 @@ app.get('/scrapper', function (req, res) {
     axios.get('https://comicbook.com/comics/news/').then(function (response) {
         var $ = cheerio.load(response.data);
 
-        $('article.category_art').each(function(i,element){
-            var title = $(element).find('h2').text();
-            var link = $(elemnt).children('a').attr('href');
-        })
-    })
-})
+        $('article.category_art').each(function (i, element) {
+            var results = {};
+
+            results.title = $(element).find('h2').text();
+            results.link = $(elemnt).children('a').attr('href');
+
+            db.Article.create(results)
+                .then(function (dbArticles) {
+                    console.log(dbArticles);
+                })
+                .catch(function (err) {
+                    console.log(err);
+                });
+        });
+        res.send('Scrapping what needed to be scraped has been scraped, so no need to scrap with the scrapper any more, Scrapper-Dapper!');
+    });
+});
 
